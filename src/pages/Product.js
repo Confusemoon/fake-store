3154114c6fdea0b1ac0de52696165ca9a6e49ee6
ZXY-0830 <3274592096@qq.com>
2025-05-29 12:53:32 -0500

    first commit for creating product-page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,24 +2,8 @@ function Product ()  {
     return (
         <section>
             <h2>Product</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium 
-                reprehenderit modi ad, nihil architecto sunt vitae, aliquid esse 
-                laboriosam voluptatum ut delectus culpa libero quisquam illum eveniet 
-                explicabo nisi tempora? Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Laudantium reprehenderit modi ad nihil architecto sunt vitae, aliquid 
-                esse laboriosam voluptatum ut delectus culpa libero quisquam illum eveniet 
-                explicabo nisi tempora.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium 
-                reprehenderit modi ad, nihil architecto sunt vitae, aliquid esse 
-                laboriosam voluptatum ut delectus culpa libero quisquam illum eveniet 
-                explicabo nisi tempora? Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Laudantium reprehenderit modi ad nihil architecto sunt vitae, aliquid 
-                esse laboriosam voluptatum ut delectus culpa libero quisquam illum eveniet 
-                explicabo nisi tempora.
-            </p>
+            <p></p>
+            <p></p>
         </section>
     );
 }
