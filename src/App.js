27938a796b842776ef63f75bd1932d9ffd45fc75
Haodi Hua <huahodi2014@gmail.com>
2025-06-02 07:12:00 -0500

    Adding Swiper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import Layout from './components/Layout';
+import Header from "./components/Header";
+import HeroBanner from "./components/Herobanner";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/notFound";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout/>}>  
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-      </Route>
-      <Route path="/404" element={<NotFound />} />
-      
-      <Route path="*" element={<Navigate to="/404" replace />} />
-    </Routes>
+    <>
+      <Header />
+      <HeroBanner />  
+      <main>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Product" element={<Product />} />
+            <Route exact path="/Cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -1,32 +1,27 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";               
+import "swiper/css/pagination";    
+import "swiper/css/navigation";    
 
-function HeroBanner() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://fakestoreapi.com/products?limit=5').then(res => {
-      setProducts(res.data);
-    });
-  }, []);
-
+const HeroBanner = () => {
   return (
-    <section className="hero-banner flex">
-      <div className="hero-text">
-        <h2>Best Products, Best Prices</h2>
-        <p>Discover the perfect product for you!</p>
-        <div className="app-buttons">
-          <button>Download for iOS</button>
-          <button>Download for Android</button>
-        </div>
-      </div>
-      <div className="hero-carousel">
-        {products.map(product => (
-          <img key={product.id} src={product.image} alt={product.title} />
-        ))}
-      </div>
-    </section>
+    <Swiper
+      modules={[Pagination, Autoplay, Navigation]} 
+      loop={true}                        
+      autoplay={{ delay: 5000 }}         
+      pagination={{ clickable: true }}   
+      navigation                        
+    >
+      <SwiperSlide>
+        <img src="banner1.jpg" alt="Banner1" className="banner-img" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="banner2.jpg" alt="Banner2" className="banner-img" />
+      </SwiperSlide>
+    </Swiper>
   );
-}
+};
 
 export default HeroBanner;
